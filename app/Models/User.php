f20f3d703db988aaf7cef531489a0809f5ed3f0c
Jardel Bazi <jardelbazi@gmail.com>
2022-07-29 19:56:36 -0300

    fix relacionamento many-to-many

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,5 +47,10 @@ class User extends Authenticatable
 	public function preference(): HasOne
 	{
 		return $this->hasOne(Preference::class);
+	}
+
+	public function permissions(): BelongsToMany
+	{
+		return $this->belongsToMany(Permission::class);
 	}
 }
